6e3bb2adf7614cc2d973d1961f5f5e928748135a
askodia <60332780+askodia@users.noreply.github.com>
2025-11-08 22:04:10 -0500

    Added Initial Match Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import LoginPage from "./scenes/Login"
 import RegisterPage from "./scenes/Register"
 import MainPage from "./scenes/Main"
 import Layout from "./components/Layout"
+import MatchPage from "./scenes/Match"
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/home" element={<HomePage />} />
+            <Route path="/match" element={<MatchPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
